2e1183c23547c66a824d439b7ce5c71b30c6e73e
ofer shaham <ofer@sergata.com>
2024-04-14 02:09:49 +0300

    show speech recognitions event names and log errors

--- src/components/SpeechAndRecognitionComponents/Dictaphone.tsx
+++ src/components/SpeechAndRecognitionComponents/Dictaphone.tsx
@@ -65,11 +65,12 @@ export const Dictaphone: React.FC = () => {
 
     renderCountRef.current += 1;
 
-
     const availableVoicesCode = useMemo<string[] | null>(() => availableVoices.map(r => r.lang), [availableVoices])
 
 
     const stopListen = useCallback((): Promise<void> => {
+        console.log('SpeechRecognition.getRecognition', SpeechRecognition.getRecognition())
+
         return SpeechRecognition.stopListening().catch(e => console.error('stopListening', e));
     }, [])
 
@@ -108,6 +109,47 @@ export const Dictaphone: React.FC = () => {
         return { language: fromLang, interimResults: isInterimResults, continuous: isContinuous }
     }, [fromLang, isContinuous, isInterimResults])
 
+    useEffect(() => {
+        const recognition = SpeechRecognition.getRecognition()
+        if (!recognition) { console.error('no recognition'); return }
+        const onError = (event: SpeechRecognitionErrorEvent): void => {
+            console.error({ status: 'error', errorCode: event.error });
+        };
+        const handleEvent = (eventData: SpeechRecognitionEvent) => {
+            console.log(`event occurred:`, eventData.type, eventData);
+        };
+        const handleEvent2 = (eventData: Event) => {
+            console.log(`event occurred:`, eventData.type, eventData);
+        };
+
+        // @ts-ignore: refer to the top `wrong SpeechRecognition-related types` comments
+        recognition.addEventListener('error', onError);
+        recognition.addEventListener('audiostart', handleEvent2);
+        recognition.addEventListener('soundstart', handleEvent2);
+        recognition.addEventListener('speechstart', handleEvent2);
+        recognition.addEventListener('speechend', handleEvent2);
+        recognition.addEventListener('soundend', handleEvent2);
+        recognition.addEventListener('audioend', handleEvent2);
+        recognition.addEventListener('result', handleEvent);
+        recognition.addEventListener('nomatch', handleEvent);
+        recognition.addEventListener('start', handleEvent2);
+        recognition.addEventListener('end', handleEvent2);
+        return () => {
+            // @ts-ignore: refer to the top `wrong SpeechRecognition-related types` comments
+            recognition.removeEventListener('error', onError);
+            recognition.removeEventListener('audiostart', handleEvent2);
+            recognition.removeEventListener('soundstart', handleEvent2);
+            recognition.removeEventListener('speechstart', handleEvent2);
+            recognition.removeEventListener('speechend', handleEvent2);
+            recognition.removeEventListener('soundend', handleEvent2);
+            recognition.removeEventListener('audioend', handleEvent2);
+            recognition.removeEventListener('result', handleEvent);
+            recognition.removeEventListener('nomatch', handleEvent);
+            recognition.removeEventListener('start', handleEvent2);
+            recognition.removeEventListener('end', handleEvent2);
+        }
+    }, [])
+
     /*
     update all refs according to state:
     */
@@ -116,6 +158,7 @@ export const Dictaphone: React.FC = () => {
 
     const startListen = useCallback((): Promise<void> => {
         try {
+
             return SpeechRecognition.startListening(listeningOptions)
         } catch (e) {
             console.error(e);
