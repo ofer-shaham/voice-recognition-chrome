cac975e95828c5eed1957e79e4238db686926452
ofer shaham <ofer@sergata.com>
2024-04-13 12:33:12 +0300

    bugfix: fix render loop

--- src/hooks/useRecording.tsx
+++ src/hooks/useRecording.tsx
@@ -1,52 +1,53 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import { MediaRecorderRecordingService } from '../services/RecordService';
 
 interface RecordingService {
-    startRecording: () => void;
-    stopRecording: () => Promise<string>;
-    cancelRecording: () => void;
+  startRecording: () => void;
+  stopRecording: () => Promise<string>;
+  cancelRecording: () => void;
 }
 
-export function useRecording() {
-    const [isRecording, setIsRecording] = useState(false);
-    const recordingInstance = useRef<RecordingService | null>(null);
+export function useRecording(allowRecording: boolean) {
+  const [isRecording, setIsRecording] = useState(false);
+  const recordingInstance = useRef<RecordingService | null>(null);
 
-    const startRecording = () => {
-        const audioContext = new AudioContext();
-        recordingInstance.current = new MediaRecorderRecordingService(audioContext);
+  const startRecording = useCallback(() => {
+    if (allowRecording) {
+      const audioContext = new AudioContext();
+      recordingInstance.current = new MediaRecorderRecordingService(audioContext);
 
-        if (recordingInstance.current) {
-            recordingInstance.current.startRecording();
-            setIsRecording(true);
-        }
-    };
+      if (recordingInstance.current) {
+        recordingInstance.current.startRecording();
+        setIsRecording(true);
+      }
+    }
+  },[allowRecording]);
 
-    const stopRecording = async () => {
-        if (recordingInstance.current) {
-            try {
-                const data = await recordingInstance.current.stopRecording();
-                setIsRecording(false);
-                // setRecordedData(data);
-                return data;
-            } catch (error) {
-                console.error('Error stopping recording:', error);
-                return ''
-            }
-        }
-        return ''
-    };
+  const stopRecording = useCallback(async () => {
+    if (recordingInstance.current) {
+      try {
+        const data = await recordingInstance.current.stopRecording();
+        setIsRecording(false);
+        return data;
+      } catch (error) {
+        console.error('Error stopping recording:', error);
+        return '';
+      }
+    }
+    return '';
+  },[]);
 
-    const cancelRecording = () => {
-        if (recordingInstance.current) {
-            recordingInstance.current.cancelRecording();
-            setIsRecording(false);
-        }
-    };
+  const cancelRecording = useCallback(() => {
+    if (recordingInstance.current) {
+      recordingInstance.current.cancelRecording();
+      setIsRecording(false);
+    }
+  },[]);
 
-    return {
-        isRecording,
-        startRecording,
-        stopRecording,
-        cancelRecording,
-    };
+  return {
+    isRecording,
+    startRecording,
+    stopRecording,
+    cancelRecording,
+  };
 }
