ed5c6b41aa0ed51115549e952bd815b708cf821b
ofer shaham <ofer@sergata.com>
2024-04-14 00:13:36 +0300

    show readme before the application starts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Todo from './components/LogAndDebugComponents/mdPresenter';
-
 import './styles/App.css';
 import { Dictaphone } from './components/SpeechAndRecognitionComponents/Dictaphone';
 
-
-
 function App() {
   const [microphoneAccess, setMicrophoneAccess] = useState(false);
+  const [showDictaphone, setShowDictaphone] = useState(false);
+  const [showMainComponent, setShowMainComponent] = useState(false);
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setTimeout(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [countdown]);
+
+  const handleClick = () => {
+    setShowMainComponent(!showMainComponent);
+  };
+
   const checkMicrophoneAccess = async () => {
     try {
       await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -21,18 +35,43 @@ function App() {
     checkMicrophoneAccess();
   }, []);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowDictaphone(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       {microphoneAccess ? (
-        <div className="App"> 
-          <Todo url="https://raw.githubusercontent.com/ofer-shaham/voice-recognition-chrome/main/README.md" />
-          <Dictaphone />
+        <div className="App">
+
+          {countdown > 0 ? (
+            <div className="countdown">
+              <h2>Countdown: {countdown}</h2>
+              <Todo url="https://raw.githubusercontent.com/ofer-shaham/voice-recognition-chrome/main/README.md" />
+
+            </div>
+
+          ) : (
+            <button className="main-button" onClick={handleClick}>
+              {showMainComponent ? 'Hide Main Component' : 'Show Main Component'}
+            </button>
+          )}
+
+          {showMainComponent && <div className="main-component">
+            {showDictaphone && <Dictaphone />}
+          </div>}
+
+
+
         </div>
       ) : (
         <button onClick={checkMicrophoneAccess}>Grant microphone access</button>
       )}
     </>
-  )
+  );
 }
 
 export default App;
