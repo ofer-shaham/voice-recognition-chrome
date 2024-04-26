71c3651b856a33c2fb8e94d8ee6e8cf077678c12
Ofer.Shaham <mostuf25562@gmail.com>
2024-04-27 01:04:46 +0300

    hide empty history component completly

--- src/components/SpeechAndRecognitionComponents/TranscriptHistory.tsx
+++ src/components/SpeechAndRecognitionComponents/TranscriptHistory.tsx
@@ -17,73 +17,75 @@ const TranscriptHistory: React.FC<TranscriptHistoryProps> = ({
   const LIMIT_ARR_CUT_FINAL = 10; // Define your desired limit for displaying transcript history
 
   return (
-    <div>
-      <p>conversation history:</p>
-      <table style={{ width: '100%' }}>
-        <tbody>
-          {finalTranscriptHistory
-            .slice(-LIMIT_ARR_CUT_FINAL)
-            .reverse()
-            .map((r, i) => (
-              <React.Fragment key={r.uuid}>
-                <tr style={{ marginBottom: '10px' }}>
-                  {/* Add margin bottom for vertical space */}
-                  <td
-                    style={{
-                      background: 'red',
-                      display: 'flex',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <button
-                      style={{ width: '100%', padding: '5px' }}
-                      onClick={() => onfreeSpeakOnly(r.finalTranscriptProxy, r.fromLang)}
+    
+      finalTranscriptHistory.length ? (<div>
+        <p>conversation history:</p>
+        <table style={{ width: '100%' }}>
+          <tbody>
+            {finalTranscriptHistory
+              .slice(-LIMIT_ARR_CUT_FINAL)
+              .reverse()
+              .map((r, i) => (
+                <React.Fragment key={r.uuid}>
+                  <tr style={{ marginBottom: '10px' }}>
+                    {/* Add margin bottom for vertical space */}
+                    <td
+                      style={{
+                        background: 'red',
+                        display: 'flex',
+                        justifyContent: 'center',
+                      }}
                     >
-                      <div style={{ display: 'flex' }}>
-                        <span style={{ flex: '20%', textAlign: 'right' }}>
-                          [{r.fromLang}]
-                        </span>
-                        <span style={{ flex: '80%' }}>{r.finalTranscriptProxy}</span>
-                      </div>
-                    </button>
-                  </td>
-                  <td
-                    style={{
-                      background: 'green',
-                      display: 'flex',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <button
-                      style={{ width: '100%', padding: '5px' }}
-                      onClick={() => onfreeSpeakOnly(r.translation, r.toLang)}
+                      <button
+                        style={{ width: '100%', padding: '5px' }}
+                        onClick={() => onfreeSpeakOnly(r.finalTranscriptProxy, r.fromLang)}
+                      >
+                        <div style={{ display: 'flex' }}>
+                          <span style={{ flex: '20%', textAlign: 'right' }}>
+                            [{r.fromLang}]
+                          </span>
+                          <span style={{ flex: '80%' }}>{r.finalTranscriptProxy}</span>
+                        </div>
+                      </button>
+                    </td>
+                    <td
+                      style={{
+                        background: 'green',
+                        display: 'flex',
+                        justifyContent: 'center',
+                      }}
                     >
-                      <div style={{ display: 'flex' }}>
-                        <span style={{ flex: '20%', textAlign: 'right' }}>
-                          [{r.toLang}]
-                        </span>
-                        <span style={{ flex: '80%' }}>{r.translation}</span>
-                      </div>
-                    </button>
-                  </td>
-                  <td style={{ background: 'blue', width: '10%', maxWidth: '10%' }}>
-                    {r.audioData && (!isBase64(r.audioData) ? (
-                      <p>{r.audioData}</p>
-                    ) : (
-                      <audio
-                        controls
-                        src={`data:audio/webm;base64,${r.audioData}`}
-                        onPlay={onBeforePlayback}
-                        onEnded={onEndPlayback}
-                      />
-                    ))}
-                  </td>
-                </tr>
-              </React.Fragment>
-            ))}
-        </tbody>
-      </table>
-    </div>
+                      <button
+                        style={{ width: '100%', padding: '5px' }}
+                        onClick={() => onfreeSpeakOnly(r.translation, r.toLang)}
+                      >
+                        <div style={{ display: 'flex' }}>
+                          <span style={{ flex: '20%', textAlign: 'right' }}>
+                            [{r.toLang}]
+                          </span>
+                          <span style={{ flex: '80%' }}>{r.translation}</span>
+                        </div>
+                      </button>
+                    </td>
+                    <td style={{ background: 'blue', width: '10%', maxWidth: '10%' }}>
+                      {r.audioData && (!isBase64(r.audioData) ? (
+                        <p>{r.audioData}</p>
+                      ) : (
+                        <audio
+                          controls
+                          src={`data:audio/webm;base64,${r.audioData}`}
+                          onPlay={onBeforePlayback}
+                          onEnded={onEndPlayback}
+                        />
+                      ))}
+                    </td>
+                  </tr>
+                </React.Fragment>
+              ))}
+          </tbody>
+        </table>
+      </div>) : null
+    
   );
 };
 
