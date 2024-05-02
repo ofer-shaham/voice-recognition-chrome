0cd2aa5f677b00d519b840579644df3259e63e13
Ofer.Shaham <mostuf25562@gmail.com>
2024-05-02 03:46:53 +0300

    rm redundunt code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './components/Intro';
-import MobileVer from './components/MobileVer';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Intro />} />
-                <Route path="/mobile" element={<MobileVer />} />
             </Routes>
         </Router>
     );
