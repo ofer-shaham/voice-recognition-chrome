af1e059771c0b50b3ecc0e8d6c25585f42e9ddb4
Ofer.Shaham <mostuf25562@gmail.com>
2024-06-03 03:08:34 +0300

    add cypress and a basic title test

--- cypress/e2e/title.cy.tsx
+++ cypress/e2e/title.cy.tsx
@@ -3,11 +3,11 @@ import { cy } from 'cypress';
 describe('Example Test Suite', () => {
   beforeEach(() => {
     // Load the page before each test
-    cy.visit('/');
+    cy.visit('http://localhost:3000')
   });
 
   it('should have the correct page title', () => {
     // Assert that the page title matches the expected value
-    cy.title().should('equal', 'My Awesome Website');
+    cy.title().should('equal', 'Chrome voice recognition');
   });
 });
