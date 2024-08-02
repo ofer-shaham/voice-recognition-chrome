ed40b7f997d9590176e1f786d94ea73878c0cb6a
Ofer.Shaham <mostuf25562@gmail.com>
2024-08-03 02:51:06 +0300

    add top margin for checkboxes

--- src/components/General/CheckboxSwitch.tsx
+++ src/components/General/CheckboxSwitch.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
-    isModeValue: boolean;
-    title: string;
-    setIsModeValue: React.Dispatch<React.SetStateAction<boolean>>;
+  isModeValue: boolean;
+  title: string;
+  setIsModeValue: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-
-const CheckBoxSwitch: React.FC<Props> = ({ isModeValue, setIsModeValue, title }) => {
-    return (
-        <div id="configuration">
-            <label>
-                <input
-                    type="checkbox"
-                    checked={isModeValue}
-                    onChange={() => setIsModeValue(!isModeValue)}
-                />
-             {title}
-            </label>
-        </div>
-    );
+const CheckBoxSwitch: React.FC<Props> = ({
+  isModeValue,
+  setIsModeValue,
+  title,
+}) => {
+  return (
+    <div className="configuration">
+      <label>
+        <input
+          type="checkbox"
+          checked={isModeValue}
+          onChange={() => setIsModeValue(!isModeValue)}
+        />
+        {title}
+      </label>
+    </div>
+  );
 };
 
 export default CheckBoxSwitch;
