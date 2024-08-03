7e17ddc70b872053d7c0bb1183cc1110c519d89a
Ofer.Shaham <mostuf25562@gmail.com>
2024-08-03 03:13:39 +0300

    lazy load routed components. fix font size on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Intro from './components/Intro';
-import YoutubeTranscriptParser from './components/YoutubeTranscriptNavigator/YoutubeTranscriptParser';
-import ProverbList from './components/Proverbs/Proverbs';
-import Footer from './components/Footer';
-import './styles/App.css'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+
+// Lazy load components
+const Intro = lazy(() => import("./components/Intro"));
+const YoutubeTranscriptParser = lazy(
+  () =>
+    import("./components/YoutubeTranscriptNavigator/YoutubeTranscriptParser")
+);
+const ProverbList = lazy(() => import("./components/Proverbs/Proverbs"));
+
+// Loading component
+const Loading = () => <div>Loading...</div>;
 
 function App() {
   return (
     <Router>
       <div className="App-content">
-        <Routes>
-
-          <Route path="/" element={<Intro />} />
-          <Route path="/youtube" element={<YoutubeTranscriptParser />} />
-          <Route path="/proverb" element={<ProverbList />} />
-
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/youtube" element={<YoutubeTranscriptParser />} />
+            <Route path="/proverb" element={<ProverbList />} />
+          </Routes>
+        </Suspense>
       </div>
       <Footer />
     </Router>
-
-
   );
 }
 
